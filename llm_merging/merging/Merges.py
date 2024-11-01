c5f17554917a7a7a36865877b70fa8c11ec03a9c
iMmOrTaL2121 <160022944+iMmOrTaL2121@users.noreply.github.com>
2024-11-01 15:40:51 +0530

    Update Merges.py

--- llm_merging/merging/Merges.py
+++ llm_merging/merging/Merges.py
@@ -15,7 +15,7 @@ import llm_merging.model.decoder_functions as decoder_functions
 import llm_merging.model.encoder_decoder_functions as encoder_decoder_functions
 
 # Set Hugging Face Auth Token (replace with a valid one)
-os.environ["HF_AUTH_TOKEN"] = "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"
+os.environ["HF_AUTH_TOKEN"] = ""#TO DO
 
 class Merges(object):
 
@@ -25,7 +25,7 @@ class Merges(object):
         self.list_models = None
 
         # Set Hugging Face Auth Token (replace with a valid one)
-        os.environ["HF_AUTH_TOKEN"] = "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"
+        os.environ["HF_AUTH_TOKEN"] = "" #TO DO
 
         self.loaded_models = None
         self.loaded_configs = None
